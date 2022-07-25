7cb10d8e56da6084a291c2db132380e96715e4fa
JC-MT <jancarlosmatias@gmail.com>
2022-07-25 10:19:01 -0400

    adds second object to model

--- Models/Transactions.js
+++ Models/Transactions.js
@@ -1,9 +1,16 @@
 const transactions = [
     {
-      item_name: "income",
+      item_name: "Income",
       amount: 1588,
       date: "08/31/2022",
       from: "employer",
+      category: "paycheck",
+    },
+    {
+      item_name: "Taxes",
+      amount: -300,
+      date: "09/05/2022",
+      from: "Me",
       category: "paycheck",
     },
   ];
